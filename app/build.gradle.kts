3ab9bbc333f2c72cafdd05841250b4b8459494ef
TeddyChoe <teddy.choe@sendbird.com>
2024-04-25 23:48:38 +0900

    remove local sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-api("com.sendbird.sdk:sendbird-chat-local:4.16.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
