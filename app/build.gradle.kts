bbcef5fe0b2cb7293587a4b4dfe71422506885d9
TeddyChoe <teddy.choe@sendbird.com>
2024-04-29 16:19:16 +0900

    update build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,6 @@ emerge {
 }
 
 dependencies {
-api("com.sendbird.sdk:sendbird-chat-local:4.16.1")
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
